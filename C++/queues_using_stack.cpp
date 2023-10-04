06ae6a50ddb015e9d1eec635c0072c91f119a29f
anushkadewangan <82151198+anushkadewangan@users.noreply.github.com>
2023-10-04 17:41:50 +0530

    Add files via upload

---
+++ C++/queues_using_stack.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Queue {
+	stack<int> s;
+	void enQueue(int x)
+	{
+		s.push(x);
+	}
+	int deQueue()
+	{
+		if (s.empty()) return -1;
+		int x = s.top();
+		s.pop();
+		if (s.empty()) return x;
+		int item = deQueue();
+		s.push(x);
+		return item;
+	}
+};
+int main()
+{
+	Queue q;
+	q.enQueue(1);
+	q.enQueue(2);
+	q.enQueue(3);
+	cout << q.deQueue() << '\n';
+	cout << q.deQueue() << '\n';
+	cout << q.deQueue() << '\n';
+	return 0;
+}
